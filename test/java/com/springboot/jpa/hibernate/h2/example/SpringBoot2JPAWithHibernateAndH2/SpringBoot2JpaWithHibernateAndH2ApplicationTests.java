0adcd0967919d224013860f4aae049947bab8a32
SheikhMuz <137673355+SheikhMuz@users.noreply.github.com>
2023-12-20 23:47:07 +0530

    Test file

---
+++ test/java/com/springboot/jpa/hibernate/h2/example/SpringBoot2JPAWithHibernateAndH2/SpringBoot2JpaWithHibernateAndH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.jpa.hibernate.h2.example.SpringBoot2JPAWithHibernateAndH2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2JpaWithHibernateAndH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
